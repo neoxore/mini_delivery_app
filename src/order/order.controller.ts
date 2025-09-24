ec37cb2a6dd16f3990610718f2739815215fc534
Danila Kovalev <neoxore@MacBook-Air-Danila.local>
2025-09-24 11:32:51 +0300

    order controllers was added

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { PaginationDtoBase } from 'src/utils/paganation.dto';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { OrderDto } from './dto/order.dto';
 
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  @Auth()
+  public async getAll(@Query("p") p: PaginationDtoBase) {
+    return await this.orderService.getAll(p);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  @Auth()
+  public async getByUserId(@CurrentUser('id') userId: string, p: PaginationDtoBase) {
+    return await this.orderService.getByUserId(userId, p);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  @Auth()
+  public async placeOrder(@CurrentUser('id') userId: string, @Body() dto: OrderDto) {
+    return await this.orderService.placeOrer(userId, dto);
+  }
+
 }
